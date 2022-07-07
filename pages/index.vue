7469354559e615c1cd3c7a36b5f7aaa61943e048
Gomulko <rafa.gomu1994@gmail.com>
2022-07-07 22:29:59 +0200

    add tabs in content

--- pages/index.vue
+++ pages/index.vue
@@ -1,6 +1,21 @@
 <template>
   <section>
-    <h1>Content</h1>
+    <tabs>
+      <tab @click="switchTab('poland')">Poland</tab>
+      <tab @click="switchTab('yourCountry')">Your Country</tab>
+      <tab @click="switchTab('search')">Search</tab>
+    </tabs>
+    <div class="content">
+      <div v-if="activeTab === 'poland'" class="poland-box">
+        <h1>Poland box</h1>
+      </div>
+      <div v-else-if="activeTab === 'yourCountry'" class="yourCountry-box">
+        <h1>Your Country</h1>
+      </div>
+      <div v-else-if="activeTab === 'search'" class="search-box">
+        <h1>search box</h1>
+      </div>
+    </div>
   </section>
 </template>
 
@@ -8,10 +23,26 @@
 import { mapState } from 'vuex';
 export default {
   name: 'IndexPage',
+  data() {
+    return {
+      activeTab: 'poland'
+    }
+  },
   computed: {
     ...mapState([
       'covidData',
     ])
   },
+  methods: {
+    switchTab(item) {
+      this.activeTab = item
+    }
+  }
 }
 </script>
+
+<style lang="scss" scoped>
+.active {
+  font-weight: bold;
+}
+</style>
